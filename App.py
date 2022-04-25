ef59bcdf84758605099958d4f9f8129a08adf47a
Angelita Santos <angelitasantos@gmail.com>
2022-04-25 14:50:03 -0300

    create file style.css and add link base.html #1

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     title = 'Home'
-    return render_template('/layout/base.html', title = title)
+    return render_template('base.html', title = title)
 
 
 if __name__ == '__main__':
