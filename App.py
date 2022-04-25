13323e227fe74e915fe38b22e8800a381d4f6129
Angelita Santos <angelitasantos@gmail.com>
2022-04-25 13:39:48 -0300

    create file base.html and render template #1

--- App.py
+++ App.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Home'
+    title = 'Home'
+    return render_template('/layout/base.html', title = title)
 
 
 if __name__ == '__main__':
     app.run(debug = True)
-
