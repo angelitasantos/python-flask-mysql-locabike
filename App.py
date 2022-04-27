32141141ecac7c9fbd70b4703a356a4e844ed293
Angelita Santos <angelitasantos@gmail.com>
2022-04-27 11:51:46 -0300

    create ModelMail and ViewMail files #3

--- App.py
+++ App.py
@@ -1,16 +1,6 @@
-from flask import Flask, render_template
+from models.ModelMail import *
+from views.ViewMail import *
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    title = 'Home'
-    return render_template('/pages/home.html', title = title)
-
-@app.route('/about')
-def about():
-    title = 'About'
-    return render_template('/pages/about.html', title = title)
 
 if __name__ == '__main__':
     app.run(debug = True)
---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import dotenv
+import os
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+secret_key = os.getenv('SECRET_KEY')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
+
+
+@app.route('/')
+def home():
+    title = 'Home'
+    return render_template('/pages/home.html', title = title)
+
+@app.route('/about')
+def about():
+    title = 'About'
+    return render_template('/pages/about.html', title = title)
