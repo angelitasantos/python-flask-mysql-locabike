daa7049728d826d9b3f349744a36e53ab43e2969
Angelita Santos <angelitasantos@gmail.com>
2022-04-29 20:30:03 -0300

    Revert "without .env"
    This reverts commit 963b516374dbf9c07ff6174f7b69960c20795429.

--- models/ModelMail.py
+++ models/ModelMail.py
@@ -1,12 +1,18 @@
 from main import *
 from flask import Flask, render_template, flash, request
 from flask_mail import Mail, Message
+import dotenv
+import os
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+usermail = os.getenv('mailuser')
+userpassword = os.getenv('mailpassword')
 
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'angelitasantos@gmail.com'
-app.config['MAIL_PASSWORD'] = 'kwwpchgfbgsusswi'
+app.config['MAIL_USERNAME'] = usermail
+app.config['MAIL_PASSWORD'] = userpassword
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
