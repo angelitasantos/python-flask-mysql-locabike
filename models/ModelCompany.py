3ccff20787ec61f389d89c6dc05381b736949288
Angelita Santos <angelitasantos@gmail.com>
2022-05-06 10:23:43 -0300

    create function register new companies #5

--- models/ModelCompany.py
+++ models/ModelCompany.py
@@ -8,3 +8,34 @@ class ModelCompany:
         pass
 
 
+    def CompanyRegister(self):
+        if request.method == 'POST':
+            aname = request.form['aname']
+            apassword = request.form['apassword']
+            aconfirmpassword = request.form['aconfirmpassword']
+            password_hash = bcrypt.generate_password_hash(apassword).decode('utf-8')
+
+            amail = request.form['amail']
+            sitedata = request.form['sitedata']
+            companies = request.form['companies']
+            stores = request.form['stores']
+            admins = request.form['admins']
+
+            try:
+                cursor = mysql.connection.cursor()
+                cursor.execute('SELECT * FROM admins WHERE amail = %s', [amail])
+                cursor.fetchone()
+                cursor.close()
+                count = cursor.rowcount
+                if count == 0 and apassword == aconfirmpassword:
+                    cursor = mysql.connection.cursor()
+                    cursor.execute('''INSERT INTO admins (aname, apassword, amail, sitedata, companies, stores, admins) 
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)''',[aname, password_hash, amail, sitedata, companies, stores, admins])
+                    mysql.connection.commit()
+                    flash('Admin Created Successfully...!!!', 'success')
+                elif count == 0 and apassword != aconfirmpassword:
+                    flash('Password and Confirm Password Dont Match...!!!', 'danger')
+                else:
+                    flash('Admin Found...!!!', 'danger')
+            except Exception as e:
+                print(e)
--- views/ViewCompany.py
+++ views/ViewCompany.py
@@ -24,14 +24,32 @@ def company_list():
 
 @app.route("/company_search", methods = ["POST","GET"])
 def company_search():
-    cursor = mysql.connection.cursor()
-    if request.method == 'POST':
-        search_word = request.form['query']
-        print(search_word)
-        if search_word != '':
-            query = "SELECT * from companies WHERE nome LIKE '%{}%' OR email LIKE '%{}%' OR telefone1 LIKE '%{}%' ORDER BY nome ASC LIMIT 20".format(search_word,search_word,search_word)
-            cursor.execute(query)
-            numrows = int(cursor.rowcount)
-            data = cursor.fetchall()
-            print(numrows)
-    return jsonify({'htmlcompanyresponse': render_template('/store/company/company_response.html', response = data, numrows = numrows)})
+    try:
+        cursor = mysql.connection.cursor()
+        if request.method == 'POST':
+            search_word = request.form['query']
+            print(search_word)
+            if search_word != '':
+                query = "SELECT * from companies WHERE nome LIKE '%{}%' OR email LIKE '%{}%' OR telefone1 LIKE '%{}%' ORDER BY nome ASC LIMIT 20".format(search_word,search_word,search_word)
+                cursor.execute(query)   
+                numrows = int(cursor.rowcount)
+                data = cursor.fetchall()
+                print(numrows)
+                return jsonify({'htmlcompanyresponse': render_template('/store/company/company_response.html', response = data, numrows = numrows)})
+    except Exception as e:
+        print(e)        
+    return render_template('/store/company/company_response.html')
+
+
+@app.route('/company_register', methods = ['GET', 'POST'])
+def company_register():
+    title = 'Company Register'
+    ModelCompany.CompanyRegister(self)
+    try:
+        if session['amail'] != '' and session['companies'] == 1:
+            return render_template('/store/company/company_register.html', title = title)
+    except Exception as e:
+        print(e)
+    return render_template('/pages/home.html', title = 'Home')
+
+
