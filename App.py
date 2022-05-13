f0fe835f1c077f6d0da27e12faadedc7d84d543c
Angelita Santos <angelitasantos@gmail.com>
2022-05-13 19:07:51 -0300

    Revert "git commit -m"
    This reverts commit 651b0777e7bb66543f0f98a524430720fc8da549.

--- App.py
+++ App.py
@@ -1,12 +1,10 @@
 from models.ModelMail import *
 from models.ModelUser import *
 from models.ModelCompany import *
-from models.ModelStore import *
 
 from views.ViewMail import *
 from views.ViewUser import *
 from views.ViewCompany import *
-from views.ViewStore import *
 
 
 if __name__ == '__main__':
--- sql.sql
+++ sql.sql
@@ -1,7 +1,7 @@
 USE heroku_b1c0a9f2a3d0817;
 
 CREATE TABLE IF NOT EXISTS admins (
-    aid INTEGER AUTO_INCREMENT,
+    aid SERIAL NOT NULL,
     aname VARCHAR(50) NOT NULL,
     apassword CHAR(60) NOT NULL,
     amail VARCHAR(50) NOT NULL,
@@ -10,13 +10,13 @@ CREATE TABLE IF NOT EXISTS admins (
     stores BOOLEAN DEFAULT 0,
     admins BOOLEAN DEFAULT 0,
     acode CHAR(6) DEFAULT 0,
-    PRIMARY KEY (aid, amail)
+    PRIMARY KEY (amail)
 );
 
 SELECT * FROM admins;
 
 CREATE TABLE IF NOT EXISTS companies (
-    id INTEGER AUTO_INCREMENT,
+    id SERIAL NOT NULL,
     nome VARCHAR(50) NOT NULL,
     razaosocial VARCHAR(150),
     tipo CHAR(10) NOT NULL DEFAULT 'JURIDICA',
@@ -34,34 +34,7 @@ CREATE TABLE IF NOT EXISTS companies (
     telefone1 CHAR(15),
     telefone2 CHAR(15),
     email VARCHAR(50),
-    CONSTRAINT PK_stores PRIMARY KEY (id, email)
+    PRIMARY KEY (email)
 );
 
 SELECT * FROM companies;
-
-CREATE TABLE IF NOT EXISTS stores (
-    id INTEGER AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    razaosocial VARCHAR(150),
-    tipo CHAR(10) NOT NULL DEFAULT 'JURIDICA',
-    cnpj CHAR(20),
-    inscest CHAR(20),
-    cpf CHAR(11),
-    rg CHAR(11),
-    endereco VARCHAR(100),
-    numero CHAR(10),
-    complemento CHAR(20),
-    bairro VARCHAR(50),
-    cidade VARCHAR(50),
-    uf CHAR(2),
-    cep CHAR(8),
-    telefone1 CHAR(15),
-    telefone2 CHAR(15),
-    email VARCHAR(50),
-    id_company INTEGER NOT NULL,
-    CONSTRAINT PK_stores PRIMARY KEY (id_company, email),
-    FOREIGN KEY (id_company)
-        REFERENCES companies (id)
-);
-
-SELECT * FROM stores;
