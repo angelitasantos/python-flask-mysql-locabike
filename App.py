651b0777e7bb66543f0f98a524430720fc8da549
Angelita Santos <angelitasantos@gmail.com>
2022-05-13 19:07:26 -0300

    git commit -m

--- App.py
+++ App.py
@@ -1,10 +1,12 @@
 from models.ModelMail import *
 from models.ModelUser import *
 from models.ModelCompany import *
+from models.ModelStore import *
 
 from views.ViewMail import *
 from views.ViewUser import *
 from views.ViewCompany import *
+from views.ViewStore import *
 
 
 if __name__ == '__main__':
