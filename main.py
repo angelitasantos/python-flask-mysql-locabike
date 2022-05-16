98bc29cc7baf3198bed86795b4886938f124d375
Angelita Santos <angelitasantos@gmail.com>
2022-05-16 20:58:12 -0300

    configure page not found #1

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ def home():
     title = 'Home'
     return render_template('/pages/home.html', title = title)
 
+
 @app.route('/about')
 def about():
     title = 'About'
     return render_template('/pages/about.html', title = title)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error_404.html'), 404
